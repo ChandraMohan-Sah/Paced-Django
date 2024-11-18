132f2e501ba2b54c6a0960b64fb3e4a843368d00
ChandraMohan-Sah <csah9628@gmail.com>
2024-11-18 14:42:29 +0545

    Added New Apps

---
+++ Paced_Django/app4_file_uploads/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class App4FileUploadsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app4_file_uploads'
---
+++ Paced_Django/app5_crud_operation/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class App5CrudOperationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app5_crud_operation'
