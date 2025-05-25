a4ca8294a904c4b4bd9165f139a693dfccc96a96
ChandraMohan-Sah <csah9628@gmail.com>
2025-05-25 10:44:23 +0545

    updated urls

--- home/api/urls.py
+++ home/api/urls.py
@@ -4,6 +4,6 @@ from .import views
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("/portfolio", views.capstone1, name="portfolio")
+    path("portfolio/", views.capstone1, name="portfolio")
     
 ]
